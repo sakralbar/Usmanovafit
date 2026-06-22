2ebe817716ca737d3096288c224f8bce87c12288
Eugene <schtaufenberg@yandex.ru>
2026-06-22 12:05:12 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// Основной JavaScript файл
+// Добавляйте сюда скрипты по мере необходимости
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Page loaded successfully');
+});
